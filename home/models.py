b9f7d8c4f15e86f23186857664111d4ba8554d3c
matteofrentano <matteocarabba@gmail.com>
2017-05-17 18:50:24 +0200

    sistem admin

--- home/models.py
+++ home/models.py
@@ -13,8 +13,9 @@ class Contenuto(models.Model):
         verbose_name="Home"
         verbose_name_plural="Modifica il contenuto della Home"
         
-    def __unicode__(self):
-        return u"Modifica il contenuto della Home"   
+    def __str__(self):
+        return 'Contenuto'
+  
     
                         
                         
@@ -35,6 +36,8 @@ class Slide(models.Model):
     admin_thumbnail.allow_tags = True
     
 class About(models.Model):   
+    titolo_about = models.TextField(max_length=100, blank=True,)
+    testo_about = models.TextField(max_length=3000, blank=True,)
     immagine_box_1 = models.FileField(max_length=500,blank=True,)
     testo_box_1 = models.TextField(max_length=3000, blank=True,)
     immagine_box_2 = models.FileField(max_length=500,blank=True,)
@@ -44,15 +47,13 @@ class About(models.Model):
     immagine_box_4 = models.FileField(max_length=500,blank=True,)
     testo_box_4 = models.TextField(max_length=3000, blank=True,) 
     immagine_box_5 = models.FileField(max_length=500,blank=True,)
-    testo_box_5 = models.TextField(max_length=3000, blank=True,)   
-    titolo_about = models.TextField(max_length=100, blank=True,)
-    testo_about = models.TextField(max_length=3000, blank=True,)
+    testo_box_5 = models.TextField(max_length=3000, blank=True,)       
     class Meta:
         verbose_name="About"
         verbose_name_plural="Modifica il contenuto della pagina About"
         
-    def __unicode__(self):
-        return u"Modifica il contenuto della pagina About"
+    def __str__(self):
+        return 'About'
         
 class Work(models.Model):   
     screen = models.FileField(max_length=500,blank=True,)
@@ -64,8 +65,8 @@ class Work(models.Model):
         verbose_name="Work"
         verbose_name_plural="Modifica il contenuto della pagina Work"
         
-    def __unicode__(self):
-        return u"Modifica il contenuto della pagina Work"
+    def __str__(self):
+        return self.titolo_work
         
         
         
