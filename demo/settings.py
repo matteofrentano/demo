adbd60517969493aee157f0738b1b68ec53bb73e
matteofrentano <matteocarabba@gmail.com>
2017-06-04 18:30:50 +0200

    tolta psw personale

--- demo/settings.py
+++ demo/settings.py
@@ -30,8 +30,8 @@ ALLOWED_HOSTS = ['*']
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'matteocarabba@gmail.com'
-EMAIL_HOST_PASSWORD = 'Quellasporca12'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
